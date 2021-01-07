c6b46dc347d0034e587f7bd948c2a42260fddc91
Viplav Jha <67279519+Viplav-Jha@users.noreply.github.com>
2021-01-07 22:33:04 +0530

    Second commit
    How to use {props} in reacts

--- src/App.js
+++ src/App.js
@@ -13,3 +13,26 @@ return (
 }
 
 export default App;
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+ // React in {props} using functional components :
+
+import React from 'react';
+import './App.css';
+import Components from './Components'
+
+const App = ()=> {
+return (
+    <div className="App">
+   <Components name="viplav" />
+   <Components name="Jack" />
+   <Components name="Jackson" />
+     
+  
+    </div>
+      
+    );
+  
+}
+
+export default App; 
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
